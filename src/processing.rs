15fcdc29a0f301e7db6c213f0785158b8803713f
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2025-03-05 11:03:19 +0000

    use hz rather than seconds

--- src/processing.rs
+++ src/processing.rs
@@ -30,7 +30,7 @@ fn raw_to_mps2(value: i16, acc_range: AccRange) -> f32 {
 
 /// Raw value to degrees per second.
 fn raw_to_dps(value: i16, gyr_range: GyrRangeVal, odr: Odr) -> f32 {
-    odr.to_seconds() * (gyr_range.to_i16() as f32) * (value as f32 / (i32::MAX as f32))
+    odr.to_hz() * (gyr_range.to_i16() as f32) * (value as f32 / (i32::MAX as f32))
 }
 
 #[derive(Debug, Clone, Copy)]
